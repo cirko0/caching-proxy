f45bed9aa673a26ddd6cace263b55632955ec327
cirko0 <cirko007ivan@gmail.com>
2025-03-06 00:22:08 +0100

    Implement core functionality and minor tweaks

--- bin/index.js
+++ bin/index.js
@@ -1,11 +1,36 @@
 #!/usr/bin/env node
 
 import { program } from "commander";
+import { clearCache, startProxyServer } from "../src/server.js";
 
+console.clear();
+
+console.log("-----------------------------------------");
 console.log("Starting CLI...");
+console.log("-----------------------------------------");
 
-program.description("hello world").action(() => {
-  console.log("a");
-});
+program
+  .name("caching-proxy")
+  .description("Caching Proxy CLI")
+  .version("1.0.0", "-v, --version", "output the current version")
+  .option("-p, --port <number>", "choose the port to run on", parseInt)
+  .option("-o, --origin <url>", "choose the origin url to proxy to")
+  .option("-c, --clear-cache", "clear the cache")
+  .action((options) => {
+    if (!options.port || !options.origin) return program.help();
+
+    if (program.clearCache) clearCache();
+
+    startProxyServer(options.port, options.origin);
+  });
+
+program.addHelpText(
+  "after",
+  `
+    Examples:
+      $ caching-proxy --port 8080 --origin http://example.com
+      $ caching-proxy -p 3000 -o https://myapi.com
+    `
+);
 
 program.parse(process.argv);
--- src/server.js
+++ src/server.js
@@ -1,11 +1,54 @@
+import axios from "axios";
 import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+let cache = {};
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+export const clearCache = () => {
+  cache = {};
+};
+
+export const startProxyServer = (port, url) => {
+  app.use(async (req, res, next) => {
+    try {
+      const newUrl = url + req.originalUrl;
+
+      if (newUrl.includes("favicon.ico")) return next();
+
+      if (cache[newUrl]) {
+        res.setHeader("X-Cache", "HIT");
+
+        // Restoring cached headers
+        Object.entries(cache[newUrl].headers).forEach(([key, value]) => {
+          res.setHeader(key, value);
+        });
+
+        console.log("Cached Response ⚡");
+        return res.send(cache[newUrl].data);
+      }
+
+      const response = await axios.get(newUrl);
+
+      res.setHeader("X-Cache", "MISS");
+
+      // Copy headers from axios response to Express response
+      Object.entries(response.headers).forEach(([key, value]) => {
+        res.setHeader(key, value);
+      });
+
+      cache[newUrl] = response;
+
+      console.log("Regular Response 🌵");
+
+      res.send(response.data);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send(error);
+    }
+  });
+
+  app.listen(port, () => {
+    console.log("Server is running on port " + port);
+  });
+};
